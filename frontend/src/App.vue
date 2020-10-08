d57edc89617bd584aecae5c8b558838223aaddd2
dzldebby <debby.lingzl@gmail.com>
2020-10-08 20:17:09 +0800

    add new student page to page folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,7 +10,8 @@ import Sidebar from './components/Sidebar.vue'
 const status = [
   { id: 1, label: 'Screening', to: '/screening', active: false },
   { id: 2, label: 'Matching', to: '/matching', active: false },
-  { id: 3, label: 'Matched', to: '/matched', active: false }
+  { id: 3, label: 'Matched', to: '/matched', active: false },
+  { id: 4, label: 'New Student', to: '/new', active: false }
 ]
 
 const profiles = [
